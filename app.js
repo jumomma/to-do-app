3ba3a9cc2b09604d6ca8a38e9a6874ab39aa7906
jumomma <jumomma@gmail.com>
2017-10-16 17:47:47 -0400

    Assignment 7 completed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
     // get the text
@@ -15,6 +15,18 @@ function onReady() {
     //create a new input
     let checkbox = document.createElement('input');
 
+    //create a delete button
+    let deleteButton = document.createElement('button');
+    //give the button text
+    var buttonText = document.createTextNode('Delete To-Do');
+    //attach the buttonText to the parent deleteButton
+    deleteButton.appendChild(buttonText);
+
+    deleteButton.addEventListener('click', event => {
+      event.preventDefault();
+      toDoList.removeChild(newLi);
+    });
+
     //set the input's type to checkbox
     checkbox.type = "checkbox";
 
@@ -24,13 +36,20 @@ function onReady() {
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    // attach delete button to the li
+    newLi.appendChild(deleteButton);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
     newToDoText.value = '';
-  });
-}
+
+
+});
+
+
+};
 
 
 window.onload = function() {
